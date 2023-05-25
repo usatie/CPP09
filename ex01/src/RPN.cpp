f50123936b272726f4de5bf044f8b9a1bf9cdfcd
Shun Usami <usatie@gmail.com>
2023-05-25 11:08:59 +0900

    Initial commit for ex01(RPN)

---
+++ ex01/src/RPN.cpp
@@ -0,0 +1,13 @@
+#include "RPN.hpp"
+#include <iostream>
+
+RPN::RPN() {}
+
+RPN::RPN(const RPN& r) {}
+
+RPN& RPN::operator =(const RPN& r) {
+	return *this ;
+}
+
+RPN::~RPN() {
+}
---
+++ ex01/src/RPN.hpp
@@ -0,0 +1,14 @@
+#ifndef RPN_HPP
+# define RPN_HPP
+
+class RPN {
+public:
+	RPN() ;
+	RPN(const RPN& r) ;
+	RPN& operator =(const RPN& r) ;
+	~RPN() ;
+protected:
+private:
+} ;
+
+#endif
